1d214c8ab6512dd3c5a6dab5b6e6a2158aacb1e2
Christine Banek <cbanek@lsst.org>
2019-05-21 18:47:41 -0500

    [DM-19838] Get metaserv stable working
    Wrong environment variable for the config file.  Get rid of
    references to unneeded volumes.

--- kube/stable/create.sh
+++ kube/stable/create.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -ex
 DAX_NAMESPACE=${DAX_NAMESPACE:-'lsst-lsp-stable-dax'}
 
-# kubectl create -f dax-metaserv-datasets-volume.yaml
-# kubectl create -f dax-metaserv-datasets-claim.yaml --namespace $DAX_NAMESPACE
 kubectl create -f dax-metaserv-deployment.yaml --namespace $DAX_NAMESPACE
 kubectl create -f dax-metaserv-service.yaml --namespace $DAX_NAMESPACE
 kubectl create -f dax-metaserv-ingress.yaml --namespace $DAX_NAMESPACE
--- kube/stable/destroy.sh
+++ kube/stable/destroy.sh
@@ -4,5 +4,3 @@ DAX_NAMESPACE=${DAX_NAMESPACE:-'lsst-lsp-stable-dax'}
 kubectl delete ingress dax-metaserv-ingress --namespace $DAX_NAMESPACE
 kubectl delete service dax-metaserv-service --namespace $DAX_NAMESPACE
 kubectl delete deployment dax-metaserv-deployment --namespace $DAX_NAMESPACE
-# kubectl delete pvc dax-metaserv-datasets-claim --namespace $DAX_NAMESPACE
-# kubectl delete pv dax-stable-metaserv-datasets-volume
