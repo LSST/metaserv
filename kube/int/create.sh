692880cd8b2fec23c6d0f45bf0d7d79c59a939e4
Kenny Lo <kennylo@lsst-bastion01.ncsa.illinois.edu>
2019-05-22 12:32:59 -0500

    Fix deployment issues on lsst-lsp-int-dax

--- kube/int/create.sh
+++ kube/int/create.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -ex
-DAX_NAMESPACE=${DAX_NAMESPACE:-'dax-int'}
+DAX_NAMESPACE=${DAX_NAMESPACE:-'lsst-lsp-int-dax'}
 
-# kubectl create -f dax-metaserv-datasets-volume.yaml
-# kubectl create -f dax-metaserv-datasets-claim.yaml --namespace $DAX_NAMESPACE
 kubectl create -f dax-metaserv-deployment.yaml --namespace $DAX_NAMESPACE
 kubectl create -f dax-metaserv-service.yaml --namespace $DAX_NAMESPACE
 kubectl create -f dax-metaserv-ingress.yaml --namespace $DAX_NAMESPACE
--- kube/int/destroy.sh
+++ kube/int/destroy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -x
-DAX_NAMESPACE=${DAX_NAMESPACE:-'dax-int'}
+DAX_NAMESPACE=${DAX_NAMESPACE:-'lsst-lsp-int-dax'}
 
 kubectl delete ingress dax-metaserv-ingress --namespace $DAX_NAMESPACE
 kubectl delete service dax-metaserv-service --namespace $DAX_NAMESPACE
 kubectl delete deployment dax-metaserv-deployment --namespace $DAX_NAMESPACE
-# kubectl delete pvc dax-metaserv-datasets-claim --namespace $DAX_NAMESPACE
-# kubectl delete pv dax-int-metaserv-datasets-volume
